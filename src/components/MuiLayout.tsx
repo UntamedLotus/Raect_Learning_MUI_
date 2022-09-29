b432750a3901b9867beb69c7c20d9e08e1bfea6e
UntamedLotus <ruttikajadhav@gmail.com>
2022-09-29 15:45:59 +0530

    1-dimensional grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Divider } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Stack
+      sx={{
+        border: "1px solid",
+      }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+      p={2}
+      margin={2}
+    >
       <Box
         //   component="span"
         sx={{
@@ -27,6 +36,6 @@ export const MuiLayout = () => {
         bgcolor="success.light"
         p={2}
       ></Box>
-    </>
+    </Stack>
   );
 };
