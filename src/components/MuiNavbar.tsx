88f718c4aeed80833b2b69f4767cec911656ab71
UntamedLotus <ruttikajadhav@gmail.com>
2022-10-01 19:51:03 +0530

    Menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -6,10 +6,25 @@ import {
   Typography,
   Stack,
   Button,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { CatchingPokemon } from "@mui/icons-material";
+import { KeyboardArrowDown } from "@mui/icons-material";
 
 export const MuiNavbar = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -24,8 +39,37 @@ export const MuiNavbar = () => {
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
           <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resources-btn"
+            onClick={handleClick}
+            aria-control={open ? "resources-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDown />}
+          >
+            Resources
+          </Button>
           <Button color="inherit">Log in</Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resources-button" }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
