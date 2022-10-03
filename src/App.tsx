e0c5f2c10dc329f69b276157c1e5a63321b53da2
UntamedLotus <ruttikajadhav@gmail.com>
2022-10-04 00:36:32 +0530

    Snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ import "./App.css";
 // import { MuiChip } from "./components/MuiChip";
 // import { MuiTooltip } from "./components/MuiTooltip";
 // import { MuiTable } from "./components/MuiTable";
-import { MuiAlert } from "./components/MuiAlert";
+// import { MuiAlert } from "./components/MuiAlert";
+import { MuiSnackbar } from "./components/MuiSnackbar";
 
 function App() {
   return (
@@ -55,7 +56,8 @@ function App() {
       {/* <MuiChip /> */}
       {/* <MuiTooltip /> */}
       {/* <MuiTable /> */}
-      <MuiAlert />
+      {/* <MuiAlert /> */}
+      <MuiSnackbar />
     </div>
   );
 }
---
+++ src/components/MuiSnackbar.tsx
@@ -0,0 +1,44 @@
+import React, { forwardRef } from "react";
+import { Snackbar, Button, Alert, AlertProps } from "@mui/material";
+
+const SnackbarAlert = React.forwardRef<HTMLDivElement, AlertProps>(
+  function SnackbarAlert(props, ref) {
+    return <Alert elevation={6} ref={ref} {...props} />;
+  }
+);
+
+export const MuiSnackbar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Submit</Button>
+      {/* <Snackbar
+        message="Form submitted successfully"
+        autoHideDuration={4000}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+      /> */}
+
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <SnackbarAlert onClose={handleClose} severity="success">
+          Form submitted successfully
+        </SnackbarAlert>
+      </Snackbar>
+    </>
+  );
+};
