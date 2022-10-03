4101fd7f70a7726193b616a1def8633e6295bbe2
UntamedLotus <ruttikajadhav@gmail.com>
2022-10-03 23:45:20 +0530

    Tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,8 @@ import "./App.css";
 // import { MuiAvatar } from "./components/MuiAvatar";
 // import { MuiBadge } from "./components/MuiBadge";
 // import { MuiList } from "./components/MuiList";
-import { MuiChip } from "./components/MuiChip";
+// import { MuiChip } from "./components/MuiChip";
+import { MuiTooltip } from "./components/MuiTooltip";
 
 function App() {
   return (
@@ -49,7 +50,8 @@ function App() {
 
       {/* <MuiBadge /> */}
       {/* <MuiList /> */}
-      <MuiChip />
+      {/* <MuiChip /> */}
+      <MuiTooltip />
     </div>
   );
 }
---
+++ src/components/MuiTooltip.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Tooltip, IconButton } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+
+export const MuiTooltip = () => {
+  return (
+    <Tooltip
+      title="Delete"
+      placement="right"
+      arrow
+      enterDelay={500}
+      leaveDelay={200}
+    >
+      <IconButton>
+        <Delete />
+      </IconButton>
+    </Tooltip>
+  );
+};
